da87f39b32372756ed7acba722197453ac5930d7
David Watson <david.s.watson11@gmail.com>
2022-11-26 13:49:59 +0000

    wishlist

---
+++ todo.R
@@ -0,0 +1,7 @@
+# Other exponential family options, with normalization constant?
+
+# Either keep continuous/categorical psi separate or maybe allow different
+# distributions for each feature? Then psi would be a list with d elements.
+# Add KDE option?
+
+# Idea: let each tree have its own synthetic dataset?
