534f1a64e3940375b81b9ab76cf7b65f83e7d93d
David Watson <david.s.watson11@gmail.com>
2022-12-05 15:15:45 +0000

    wishlist

--- todo.R
+++ todo.R
@@ -5,3 +5,17 @@
 # Add KDE option?
 
 # Idea: let each tree have its own synthetic dataset?
+
+# Bayesian version with some prior? Could help with zero-density events when
+# a level is unobserved, for instance, without being ruled out per se.
+
+# Localized version, where we only regrow nodes that split too well
+
+# Fully adversarial with alternating rounds at each node?
+
+# Allow arbitrary queries for total evidence, marginals, MAP, etc.
+# e.g. p(x1), p(x1, x2), p(x1 | x2). Problem: need to accommodate both
+# equalities and inequalities on either side of the conditioning bar.
+# Also conjunctions and disjunctions.
+
+# Add conditional sampling option to forge
