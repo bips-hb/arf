ac0bf8c60736a152d5b920e49710bad6e42429a5
Marvin Wright <github@wrig.de>
2024-01-05 12:11:38 +0100

    don't parallelize in tests

--- tests/testthat/test-arguments.R
+++ tests/testthat/test-arguments.R
@@ -1,4 +1,4 @@
 test_that("FORDE works with alpha>0", {
-  arf <- adversarial_rf(iris)
-  expect_silent(forde(arf, iris, alpha = 0.01))
+  arf <- adversarial_rf(iris, parallel = FALSE)
+  expect_silent(forde(arf, iris, parallel = FALSE, alpha = 0.01))
 })
